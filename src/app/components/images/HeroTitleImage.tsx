14b3158d8de6cc66df4c3dfe5b5d96e90c6b7f8f
tectanagrand <rtektano@gmail.com>
2026-06-29 14:51:43 +0700

    fix : ascii title image

--- src/app/components/images/HeroTitleImage.tsx
+++ src/app/components/images/HeroTitleImage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroTitleImage = () => {
   return (
-  <pre className="text-[2.0pt] md:text-[10.01pt] text-gray-50 leading-none whitespace-pre-wrap overflow-hidden md:w-[780px] w-[156px] m-2">                                                                                                                                                                                          
+  <pre className="text-[3pt] md:text-[10.01pt] text-gray-50 leading-none whitespace-pre-wrap overflow-hidden md:w-[780px] w-[235px] my-2 md:mx-2">                                                                                                                                                                                          
  ▄▄▄▄▄    ▄▄   ▄▄▄▄▄▄   ▄▄   ▄▄▄▄▄▄ ▄            ▄▄▄▄▄▄▄ ▄▄▄▄▄▄ ▄    ▄▄▄▄▄▄▄▄   ▄▄   ▄▄   ▄  ▄▄▄▄ 
  █   ▀█   ██   █        ██   █      █               █    █      █  ▄▀    █      ██   █▀▄  █ ▄▀  ▀▄
  █▄▄▄▄▀  █  █  █▄▄▄▄▄  █  █  █▄▄▄▄▄ █               █    █▄▄▄▄▄ █▄█      █     █  █  █ █▄ █ █    █
